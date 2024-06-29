2c22fe74f7506a0e2f05427b3d0a2cc47836b80b
Nguyen Pham Tuyen <phamtuyen121314@gmail.com>
2024-06-29 12:01:01 +0700

    Full Chi Tiet

--- CakeShop/obj/Debug/net8.0/CakeShop.AssemblyInfo.cs
+++ CakeShop/obj/Debug/net8.0/CakeShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CakeShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5682214ed17d0229b647db2e72f42bbb73f9f90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c52421e56168cc3afeee45c0f35b5d7adae1c9b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CakeShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CakeShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
